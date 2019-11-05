4a3c9d6d165dcdab48fd20fcfa1f10bb16ee43a4
Ruiyu <ruiyuwu1998@gmail.com>
2019-11-05 16:52:18 -0500

    Add language count column to data and created rating-language route

--- app.py
+++ app.py
@@ -9,8 +9,17 @@ data = pd.read_parquet('data/games.parq')
 @app.route('/names', methods=['GET'])
 def names():
     names = data['Name']
-    json_data = names.to_json(orient='index')
-    return json_data
+    json_names = names.to_json(orient='index')
+    return json_names
+
+@app.route('/rating-language', methods=['GET'])
+def rating_language():
+    games = data[['Name', 'Average User Rating', 'Language Count']]
+    games = games[games['Average User Rating'] != 0.0]
+    json_games = games.to_json(orient='index')
+    return json_games
+
+# @app.route('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
