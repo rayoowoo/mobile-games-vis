59605fc9765af4eea568febd52aa4c7a11c9482b
Ruiyu <ruiyuwu1998@gmail.com>
2019-11-05 16:19:52 -0500

    Add Flask app

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 from flask import Flask
+from fastparquet import ParquetFile
+from datetime import datetime
+import pandas as pd
 
 app = Flask(__name__)
+data = pd.read_parquet('data/games.parq')
 
+@app.route('/names', methods=['GET'])
+def names():
+    names = data['Name']
+    json_data = names.to_json(orient='index')
+    return json_data
+
+if __name__ == "__main__":
+    app.run(debug=True)
